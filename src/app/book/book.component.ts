e0dfb810efcfb3f46ba1d5dd3919f277e3871e09
AndrijanaJ00 <andrijana.jovanovic.3918@metropolitan.ac.rs>
2022-04-16 18:59:38 +0200

    get Book

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-book',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookComponent implements OnInit {
 
-  constructor() { }
+  type = "";
+  id = "";
+  url = "";
+  books: any;
+  book: any;
+
+  constructor(private rout: ActivatedRoute, private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.type = this.rout.snapshot.params['type'];
+    this.id = this.rout.snapshot.params['id'];
+    if (this.type === 'trending') {
+      this.url = 'http://localhost:4200/assets/data/trending-movies.json';
+    }
+    if (this.type === 'theatre') {
+      this.url = 'http://localhost:4200/assets/data/theatre-movies.json';
+    }
+    if (this.type === 'popular') {
+      this.url = 'http://localhost:4200/assets/data/popular-movies.json';
+    }
+    this.getBook();
   }
 
+  getBook(){
+    this.http.get(this.url).subscribe((books) => {
+      this.books = books;
+      let index = this.books.findIndex(
+        (book: { id: string }) => book.id == this.id
+      );
+      if (index > -1) {
+        this.book = this.books[index];
+      }
+    });
+  }
 }
